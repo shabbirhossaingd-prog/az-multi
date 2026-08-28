9ffdd6848bd135df763486ffa3666ad65c5715ea
shabbirhossaingd-prog <shabbirhossain.gd@gmail.com>
2026-08-29 01:07:09 +0600

    Mount ads manager in workspace

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import AdsDock from './AdsManager.jsx'
 import './styles.css'
 import './phase2.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <AdsDock />
   </React.StrictMode>,
 )
